66040f5e0eb8ba76c401925ae3103602873f40c9
Hngati <145719995+Hngati@users.noreply.github.com>
2023-10-17 00:00:03 -0400

    Task 3

--- task7.java
+++ task7.java
@@ -1,7 +1,7 @@
 public class task7 {
     public static void main(String[] args) {
-        for (int i = 5; i >= 1; i--) {
-            for (int j = 1; j <= i; j++) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
                 System.out.print("*");
             }
             System.out.println();
