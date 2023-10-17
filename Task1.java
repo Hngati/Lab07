6e8a7a2d234cbb4f70c8fe864168c18ebc9e3f75
Hngati <145719995+Hngati@users.noreply.github.com>
2023-10-16 23:39:11 -0400

    Task 1

--- Task1.java
+++ Task1.java
@@ -1,5 +1,5 @@
-public class GettingLoopy {
-    public static class GettingLoopy1 {
+public class Task1 {
+    public static class GettingLoopy {
         public static void main(String[] args) {
             for (int i = 0; i <= 30; i++) {
                 System.out.println(i);
--- Task2.java
+++ Task2.java
@@ -1,4 +1,4 @@
-public class GettingLoopy2 {
+public class Task2 {
     public static void main(String[] args) {
         for (int i = 30; i >= 0; i--) {
             System.out.println(i);
